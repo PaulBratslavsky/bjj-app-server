581a133ed953de949c2c8f70dda43bc54bd1f0df
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2023-11-26 15:26:00 -0600

    update

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,17 +1,18 @@
-const path = require('path');
+const parse = require('pg-connection-string').parse;
+
+const { host, port, database, user, password } = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'postgres');
 
   const connections = {
     postgres: {
       connection: {
-        host: env("DATABASE_HOST", "127.0.0.1"),
-        port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        user: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
-        schema: env("DATABASE_SCHEMA", "public"), // Not required
+        host,
+        port,
+        database,
+        user,
+        password,
         ssl: {
           ca: env("DATABASE_CA"),
         },
